a078fbe7a3e5da78b73c975fa518e995ca4ac6d5
Aditya Sood <adityasood314@gmail.com>
2024-05-11 11:20:49 +0530

    Create MapView screen.

---
+++ app/src/main/java/com/example/nithguide/ActivityMapView.java
@@ -0,0 +1,37 @@
+package com.example.nithguide;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.nithguide.databinding.ActivityMapViewBinding;
+
+public class ActivityMapView extends AppCompatActivity {
+
+    private ActivityMapViewBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMapViewBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        String source = getIntent().getStringExtra("SOURCE");
+        String destination = getIntent().getStringExtra("DESTINATION");
+
+
+        binding.webView.getSettings().setJavaScriptEnabled(true);
+        if (source == null || destination == null) return;
+        String url = "https://www.google.com/maps/dir/" + source + "/" + destination;
+        binding.webView.loadUrl(url);
+    }
+}
---
+++ app/src/main/java/com/example/nithguide/ActivitySplash.java
@@ -0,0 +1,78 @@
+package com.example.nithguide;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+        // Check for location permission
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            // Permission is granted, start location updates
+            startActivity(new Intent(this,MainActivity.class));
+
+        }
+
+        // Define location callback
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, start location updates
+                startActivity(new Intent(this,MainActivity.class));
+            } else {
+                // Permission denied, handle accordingly
+
+            }
+        }
+    }
+
+
+
+
+
+}
